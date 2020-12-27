9712d532298861bc39fe365a6ea9d0a6abc7b4db
Michal <desk1123@gmail.com>
2020-12-27 19:24:50 +0100

    dodano podstawa autoryzacje jwt oraz dishes endpoint

---
+++ api/api/pizza_app/views.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets, status
+from rest_framework import generics, permissions
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from api.pizza_app.models import Dish
+from api.pizza_app.serializers import UserSerializer, RegisterSerializer, DishSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DishViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Dish.objects.all().order_by('id')
+    serializer_class = DishSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class Register(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "User Created Successfully.",
+        })
