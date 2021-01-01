0124a2e9f9e4cb7629488fbc6bf86e258d6a48be
Michal <desk1123@gmail.com>
2021-01-01 12:25:21 +0100

    Change model and endpoints

--- api/api/pizza_app/views.py
+++ api/api/pizza_app/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework import generics, permissions
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, BasePermission
 from rest_framework.response import Response
 
 from api.pizza_app.models import Dish
@@ -17,13 +17,29 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
+# SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
+#
+#
+# class IsAuth(BasePermission):
+#     """
+#     The request is authenticated as a user, or is a read-only request.
+#     """
+#
+#     def has_permission(self, request, view):
+#         return bool(
+#             request.method in SAFE_METHODS or
+#             request.user and
+#             request.user.is_authenticated
+#         )
+
+
 class DishViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Dish.objects.all().order_by('id')
     serializer_class = DishSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
